155c850094eaedfea517661e2c3c3d71c4df845f
Gapur Kassym <gapur.kassym@gmail.com>
2021-09-07 10:50:29 +0600

    feat(app): init

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Text</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+});
+
+export default App;
