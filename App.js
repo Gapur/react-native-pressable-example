75268a9d6bed8293f1f6e79e60ade5aec52bb903
Gapur Kassym <gapur.kassym@gmail.com>
2021-09-07 12:24:53 +0600

    feat(app): implement pressable components

--- App.js
+++ App.js
@@ -1,18 +1,110 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Pressable } from 'react-native';
 
 function App() {
+  const [highlightPressed, setHighlightPressed] = useState(0);
+  const [opacityPressed, setOpacityPressed] = useState(0);
+  const [withoutFeedbackPressed, setWithoutFeedbackPressed] = useState(0);
+
   return (
-    <SafeAreaView>
-      <Text>Text</Text>
-    </SafeAreaView>
+    <View style={styles.screen}>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>React Native Pressable</Text>
+        </View>
+      </SafeAreaView>
+
+      <View style={styles.content}>
+        <View style={styles.buttonRow}>
+          <Text style={styles.label}>Pressable as TouchableHiglight</Text>
+          <Pressable
+            onPress={() => setHighlightPressed(highlightPressed + 1)}
+            style={() => [styles.button]}>
+            <Text style={styles.buttonText}>
+              {`${highlightPressed} pressed`}
+            </Text>
+          </Pressable>
+        </View>
+
+        <View style={styles.buttonRow}>
+          <Text style={styles.label}>Pressable as TouchableOpacity</Text>
+          <Pressable
+            onPress={() => setOpacityPressed(opacityPressed + 1)}
+            style={({ pressed }) => [
+              styles.button,
+              { opacity: pressed ? 0.5 : 1 },
+            ]}>
+            <Text style={styles.buttonText}>{`${opacityPressed} pressed`}</Text>
+          </Pressable>
+        </View>
+
+        <View style={styles.buttonRow}>
+          <Text style={styles.label}>
+            Pressable as TouchableWithoutFeedback
+          </Text>
+          <Pressable
+            onPress={() =>
+              setWithoutFeedbackPressed(withoutFeedbackPressed + 1)
+            }
+            style={({ pressed }) => [
+              styles.button,
+              {
+                backgroundColor: pressed ? '#008080' : '#a4c936',
+                opacity: pressed ? 0.5 : 1,
+              },
+            ]}>
+            <Text style={styles.buttonText}>
+              {`${withoutFeedbackPressed} pressed`}
+            </Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  screen: {
+    flex: 1,
+    backgroundColor: '#f2f2fC',
+  },
+  safeArea: {
+    backgroundColor: '#62d1bc',
+  },
+  header: {
+    height: 50,
+    backgroundColor: '#62d1bc',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    color: '#ffffff',
+    fontSize: 20,
+  },
+  content: {
+    flex: 1,
+  },
+  buttonRow: {
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    paddingTop: 28,
+  },
+  button: {
+    paddingVertical: 12,
+    backgroundColor: '#a4c936',
+    borderRadius: 8,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#ffffff',
+    fontSize: 15,
+    fontWeight: '600',
+  },
+  label: {
+    fontSize: 15,
+    fontWeight: '700',
+    color: '#2C2C2C',
+    paddingVertical: 12,
   },
 });
 
